312c970024abc1694bdf210b0ed7ff00c34b099f
Alexander Bol <alexbol@FRONTLINE-PCB.COM>
2018-12-12 13:56:09 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c9caee189bca00d4653763ae59ff0412.js"
+  "/precache-manifest.3f3a687befa5d4c3d0c8c3ddecc86850.js"
 );
 
 workbox.clientsClaim();
