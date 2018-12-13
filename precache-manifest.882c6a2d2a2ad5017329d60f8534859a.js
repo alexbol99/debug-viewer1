46a6e27a138c42b895167a54448f0bc93ee46b65
Alexander Bol <alexbol@FRONTLINE-PCB.COM>
2018-12-13 12:20:01 +0200

    Updates

--- precache-manifest.882c6a2d2a2ad5017329d60f8534859a.js
+++ precache-manifest.882c6a2d2a2ad5017329d60f8534859a.js
@@ -12,12 +12,12 @@ self.__precacheManifest = [
     "url": "/static/media/fa-solid-900.1d220cf9.svg"
   },
   {
-    "revision": "58c329e6fbef0697699c",
-    "url": "/static/js/1.58c329e6.chunk.js"
+    "revision": "362a8a0c00ddf4454627",
+    "url": "/static/js/1.362a8a0c.chunk.js"
   },
   {
-    "revision": "997f6b1819184c54248de33c81b7bb0c",
-    "url": "/static/media/fa-solid-900.997f6b18.woff"
+    "revision": "c862e94cbef741d18838774587e3c49d",
+    "url": "/static/media/fa-brands-400.c862e94c.svg"
   },
   {
     "revision": "a5e7ef6aa2d379b4110c",
@@ -32,8 +32,8 @@ self.__precacheManifest = [
     "url": "/static/js/4.a99f5888.chunk.js"
   },
   {
-    "revision": "bcaf14fe3b413b4722d5",
-    "url": "/static/js/runtime~main.bcaf14fe.js"
+    "revision": "9cbddf587bc68972dca3",
+    "url": "/static/js/runtime~main.9cbddf58.js"
   },
   {
     "revision": "921f1150167369cf4c400135a4905728",
@@ -44,59 +44,59 @@ self.__precacheManifest = [
     "url": "/static/media/fa-brands-400.ab673bac.ttf"
   },
   {
-    "revision": "9d56249d09070f656a1c52e566448f0d",
-    "url": "/static/media/fa-brands-400.9d56249d.woff"
+    "revision": "491a96d8188670aff6f5e8ae4e29ed6a",
+    "url": "/static/media/fa-regular-400.491a96d8.eot"
   },
   {
     "revision": "0353147160dd5e15058f",
     "url": "/static/js/main.03531471.chunk.js"
   },
   {
-    "revision": "c862e94cbef741d18838774587e3c49d",
-    "url": "/static/media/fa-brands-400.c862e94c.svg"
-  },
-  {
     "revision": "4f8bb28722068f7b666582a39c7035b8",
     "url": "/static/media/fa-regular-400.4f8bb287.woff"
-  },
-  {
-    "revision": "cf6008d396082c09c3dd4907de9f3941",
-    "url": "/static/media/fa-regular-400.cf6008d3.woff2"
-  },
-  {
-    "revision": "491a96d8188670aff6f5e8ae4e29ed6a",
-    "url": "/static/media/fa-regular-400.491a96d8.eot"
   },
   {
     "revision": "e6ff1c5d13b7786272782029310c3615",
     "url": "/static/media/fa-regular-400.e6ff1c5d.ttf"
   },
   {
-    "revision": "1dc5b6dd4bf409a6f919be38603f76a0",
-    "url": "/static/media/fa-solid-900.1dc5b6dd.woff2"
-  },
-  {
     "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
     "url": "/static/media/fa-regular-400.b5a61b22.svg"
   },
   {
-    "revision": "63726a69fa60cb67459140ccaf679f96",
-    "url": "/static/media/fa-solid-900.63726a69.ttf"
+    "revision": "cf6008d396082c09c3dd4907de9f3941",
+    "url": "/static/media/fa-regular-400.cf6008d3.woff2"
   },
   {
     "revision": "20b351a6af2d523589fd193785e7d7f0",
     "url": "/static/media/fa-solid-900.20b351a6.eot"
   },
   {
+    "revision": "1dc5b6dd4bf409a6f919be38603f76a0",
+    "url": "/static/media/fa-solid-900.1dc5b6dd.woff2"
+  },
+  {
+    "revision": "997f6b1819184c54248de33c81b7bb0c",
+    "url": "/static/media/fa-solid-900.997f6b18.woff"
+  },
+  {
+    "revision": "63726a69fa60cb67459140ccaf679f96",
+    "url": "/static/media/fa-solid-900.63726a69.ttf"
+  },
+  {
+    "revision": "9d56249d09070f656a1c52e566448f0d",
+    "url": "/static/media/fa-brands-400.9d56249d.woff"
+  },
+  {
     "revision": "a5e7ef6aa2d379b4110c",
     "url": "/static/css/2.82e90985.chunk.css"
   },
   {
-    "revision": "58c329e6fbef0697699c",
+    "revision": "362a8a0c00ddf4454627",
     "url": "/static/css/1.5a2c7935.chunk.css"
   },
   {
-    "revision": "1ccd0b6fce14c52715f42510dd7075d8",
+    "revision": "288694fcb4dc9302b01af5e071e36bcd",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6944326e1df8d5dea0fe90ef3dd6cb26.js"
+  "/precache-manifest.882c6a2d2a2ad5017329d60f8534859a.js"
 );
 
 workbox.clientsClaim();
