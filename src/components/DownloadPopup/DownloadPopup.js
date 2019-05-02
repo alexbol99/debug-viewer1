c2c4ddbec548fc3e505611dd559eba4195489eac
alexbol99 <alexbol99@gmail.com>
2019-05-02 23:47:42 +0300

    - upgrade to react-scripts 3.0, try hooks

--- src/components/DownloadPopup/DownloadPopup.js
+++ src/components/DownloadPopup/DownloadPopup.js
@@ -1,41 +1,45 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import ModalPopup from "../UI/ModalPopup/ModalPopup";
 import {createXMLString} from "../../dataParsers/createXML";
 
 import classes from "./DownloadPopup.module.css";
 
-class DownloadPopup extends Component {
-    render() {
-        let downloadData = this.props.layers.map( layer => {
+const DownloadPopup = (props) => {
+    const [downloadData, setDownloadData] = useState([]);
+
+    useEffect( () => {
+        const newDownloadData = props.layers.map(layer => {
             return layer.displayed ? {
                 filename: `${layer.name}.xml`,
                 xmlString: createXMLString(layer.shapes)
             } : null
         });
-        return this.props.showPopup ? (
-            <ModalPopup
-                showPopup={this.props.showPopup}
-                closePopup={this.props.closePopup}
-            >
-                <div className={classes.DownloadPopup}>
-                    <h5>Click to the following links to start download</h5>
-                    <ul>
+        setDownloadData(newDownloadData);
+    }, [props.layers]);
 
-                        {downloadData.map(data =>
-                            data === null ? null :
+    return props.showPopup ? (
+        <ModalPopup
+            showPopup={props.showPopup}
+            closePopup={props.closePopup}
+        >
+            <div className={classes.DownloadPopup}>
+                <h5>Click to the following links to start download</h5>
+                <ul>
+
+                    {downloadData.map(data =>
+                        data === null ? null :
                             <li>
                                 <a href={'data:text/json;charset=utf-8,' + encodeURIComponent(data.xmlString)}
                                    download={data.filename}>
                                     {data.filename}
                                 </a>
                             </li>
-                        )}
+                    )}
 
-                    </ul>
-                </div>
-            </ModalPopup>
-        ) : null;
-    }
-}
+                </ul>
+            </div>
+        </ModalPopup>
+    ) : null;
+};
 
 export default DownloadPopup;
